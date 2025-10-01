2c91a4a55063296846f5ae2e878ed14e73c63203
Jin Yi <jhy8406@g.rit.edu>
2025-10-01 14:28:23 -0400

    commented out server.js temporarily

--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
-const http = require('http');
+// const http = require('http');
 
-const port = process.env.PORT || process.env.NODE_PORT || 3000;
+// const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
 // const onRequest = (request, response) => {
 
 // };
 
-
-
-http.createServer(onRequest).listen(port, () => {
-    console.log(`Listening on 127.0.0.1: ${port}`);
-});
+// http.createServer(onRequest).listen(port, () => {
+//   console.log(`Listening on 127.0.0.1: ${port}`);
+// });
